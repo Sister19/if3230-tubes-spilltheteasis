c24fc09ad24a48c46338025b92eaefb19328d5e7
ranjabi <fikriranjabi02@gmail.com>
2023-05-18 19:16:11 +0700

    feat: template dari spek

---
+++ server.py
@@ -0,0 +1,27 @@
+from address       import Address
+from raft          import RaftNode
+from xmlrpc.server import SimpleXMLRPCServer
+from app           import MessageQueue
+
+
+
+def start_serving(addr: Address, contact_node_addr: Address):
+    print(f"Starting Raft Server at {addr.ip}:{addr.port}")
+    with SimpleXMLRPCServer((addr.ip, addr.port)) as server:
+        server.register_introspection_functions()
+        server.register_instance(RaftNode(MessageQueue(), addr, contact_node_addr))
+        server.serve_forever()
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("server.py <ip> <port> <opt: contact ip> <opt: contact port>")
+        exit()
+
+    contact_addr = None
+    if len(sys.argv) == 5:
+        contact_addr = Address(sys.argv[3], int(sys.argv[4]))
+    server_addr = Address(sys.argv[1], int(sys.argv[2]))
+
+    start_serving(server_addr, contact_addr)
